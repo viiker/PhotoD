d68eb8c8d20b13fd1809e5bab16c8cd7405fa31d
viiker <roger_ni@outlook.com>
2017-11-02 13:58:30 +0800

    新建微速照片发布项目

---
+++ main/java/com/viisoo/PhotoD/Constants.java
@@ -0,0 +1,16 @@
+package com.viisoo.PhotoD;
+
+/**
+ * Created by dam on 2014/11/20.
+ */
+public interface Constants {
+    String ACCESS_KEY = "lR3PhcJgTH9U1JjAXMFYSC-2CtRiTmVzpMTd6iGW";
+    String SECRET_KEY = "XXvfkw4cw5aSl_rUc149f5zdZILBlsk4ttfimSjW";
+    String BUCKET_NAME = "akdd";
+    String DOMAIN = "akdd.qiniudn.com";
+    String PHOTO_IN_PATH = "c:/Weisu/photoInPath";
+    String PHOTO_OUT_PATH = "c:/Weisu/photoOutPath";
+    double X_SHIFT = 0.8;
+    double Y_SHIFT = 0.2;
+    int PHOTO_SIZE = 1200;
+}
